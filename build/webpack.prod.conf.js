c87bb0c25790ab4f3b038b2ff000ae6913a538ca
Asper <asperwon@gmail.com>
2018-03-07 14:52:47 +0800

    modify publishpath to relative

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -43,7 +43,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('[name].[contenthash].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
